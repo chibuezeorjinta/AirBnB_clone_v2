3f393f9442cda06e2f662e8a79fc16bbd89cf619
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-04-25 13:17:41 +0000

    compress the payload

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+# use a fabfile to compares the payload.
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
+    """Create a tar gzipped archive of the entire directory web_static."""
+    DT = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(DT.year,
+                                                         DT.month,
+                                                         DT.day,
+                                                         DT.hour,
+                                                         DT.minute,
+                                                         DT.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
